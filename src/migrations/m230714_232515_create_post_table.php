bcab8e0c049439e5c7b8f7c525e1bcf15387afd8
Radim Mifka <radimmifka@gmail.com>
2023-07-15 02:56:10 +0200

    Add forum-post migration

---
+++ src/migrations/m230714_232515_create_post_table.php
@@ -0,0 +1,156 @@
+<?php
+
+/**
+ * @author kazda01, mifka01
+ */
+
+use yii\db\Migration;
+
+class m230714_232515_create_post_table extends Migration
+{
+
+    private $table_name = 'forum-post';
+
+    public function safeUp()
+    {
+        $table_options = Yii::$app->params['migrationTableOptions'] ?? null;
+        $user_table_name = Yii::$app->params['userTableName'] ?? 'user';
+
+        if ($this->db->driverName === 'mysql' && $table_options === null) {
+            $table_options = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable("{{%{$this->table_name}}}", [
+            'id' => $this->primaryKey(),
+            'fk_thread' => $this->integer()->notNull(),
+            'fk_parent' => $this->integer()->defaultValue(null),
+            'content' => $this->text()->notNull(),
+            'status' => $this->smallInteger()->notNull()->defaultValue(0),
+            'created_by' => $this->integer()->notNull(),
+            'updated_by' => $this->integer()->notNull(),
+            'created_at' => $this->timestamp(),
+            'updated_at' => $this->timestamp(),
+        ], $table_options);
+
+        // creates index for column `fk_parent`
+        $this->createIndex(
+            "{{%idx-{$this->table_name}-fk_parent}}",
+            "{{%{$this->table_name}}}",
+            'fk_parent'
+        );
+
+        // creates index for column `id`
+        $this->createIndex(
+            "{{%idx-{$this->table_name}-fk_thread}}",
+            "{{%{$this->table_name}}}",
+            'fk_thread'
+        );
+
+        // creates index for column `created_by`
+        $this->createIndex(
+            "{{%idx-{$this->table_name}-created_by}}",
+            "{{%{$this->table_name}}}",
+            'created_by'
+        );
+
+        // creates index for column `updated_by`
+        $this->createIndex(
+            "{{%idx-{$this->table_name}-updated_by}}",
+            "{{%{$this->table_name}}}",
+            'updated_by'
+        );
+
+        // add foreign key for this parent table
+        $this->addForeignKey(
+            "{{%fk-{$this->table_name}-fk_parent}}",
+            "{{%{$this->table_name}}}",
+            'fk_parent',
+            "{{%{$this->table_name}}}",
+            'id',
+            'CASCADE'
+        );
+        // add foreign key for fk_thread
+        $this->addForeignKey(
+            "{{%fk-{$this->table_name}-fk_thread}}",
+            "{{%{$this->table_name}}}",
+            'fk_thread',
+            "{{%forum-thread}}",
+            'id',
+            'CASCADE'
+        );
+
+        // add foreign key for table `{{%user}}`
+        $this->addForeignKey(
+            "{{%fk-{$this->table_name}-created_by}}",
+            "{{%{$this->table_name}}}",
+            'created_by',
+            "{{%{$user_table_name}}}",
+            'id',
+            'CASCADE'
+        );
+
+        // add foreign key for table `{{%user}}`
+        $this->addForeignKey(
+            "{{%fk-{$this->table_name}-updated_by}}",
+            "{{%{$this->table_name}}}",
+            'updated_by',
+            "{{%{$user_table_name}}}",
+            'id',
+            'CASCADE'
+        );
+    }
+
+    public function safeDown()
+    {
+
+        // drops foreign key for parent table
+        $this->dropForeignKey(
+            "{{%fk-{$this->table_name}-fk_parent}}",
+            "{{%{$this->table_name}}}",
+        );
+
+        // drops index for column `fk_parent`
+        $this->dropIndex(
+            "{{%idx-{$this->table_name}-fk_parent}}",
+            "{{%{$this->table_name}}}",
+        );
+
+        // drops foreign key for thread
+        $this->dropForeignKey(
+            "{{%fk-{$this->table_name}-fk_thread}}",
+            "{{%{$this->table_name}}}",
+        );
+
+        // drops index for column `thread`
+        $this->dropIndex(
+            "{{%idx-{$this->table_name}-fk_thread}}",
+            "{{%{$this->table_name}}}",
+        );
+
+        // drops foreign key for table `{{%user}}`
+        $this->dropForeignKey(
+            "{{%fk-{$this->table_name}-created_by}}",
+            "{{%{$this->table_name}}}",
+        );
+
+        // drops index for column `created_by`
+        $this->dropIndex(
+            "{{%idx-{$this->table_name}-created_by}}",
+            "{{%{$this->table_name}}}",
+        );
+
+        // drops foreign key for table `{{%user}}`
+        $this->dropForeignKey(
+            "{{%fk-{$this->table_name}-updated_by}}",
+            "{{%{$this->table_name}}}",
+        );
+
+        // drops index for column `updated_by`
+        $this->dropIndex(
+            "{{%idx-{$this->table_name}-updated_by}}",
+            "{{%{$this->table_name}}}",
+        );
+
+        $this->dropTable("{{%{$this->table_name}}}");
+    }
+}
