6b6a1d74836cdff675126156746050e6f35c4178
Tondina <antoninkazda@seznam.cz>
2023-07-15 21:20:21 +0200

    Add User model

---
+++ src/models/User.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace rats\forum\models;
+
+use rats\forum\ForumModule;
+use Yii;
+
+/**
+ * This is the model class for table "forum_user".
+ *
+ * @property int $id
+ * @property string $username
+ * @property string|null $email
+ * @property string|null $real_name
+ * @property int $status
+ * @property string|null $signature
+ * @property int $created_by
+ * @property int $updated_by
+ * @property string|null $created_at
+ * @property string|null $updated_at
+ *
+ * @property ForumModule::userClass $createdBy
+ * @property ForumModule::userClass $updatedBy
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'forum_user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'created_by', 'updated_by'], 'required'],
+            [['status', 'created_by', 'updated_by'], 'integer'],
+            [['signature'], 'string'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['username', 'email', 'real_name'], 'string', 'max' => 191],
+            [['id'], 'exist', 'skipOnError' => true, 'targetClass' => ForumModule::getInstance()->userClass, 'targetAttribute' => ['id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'username' => Yii::t('app', 'Username'),
+            'email' => Yii::t('app', 'Email'),
+            'real_name' => Yii::t('app', 'Real name'),
+            'status' => Yii::t('app', 'Status'),
+            'signature' => Yii::t('app', 'Signature'),
+            'created_by' => Yii::t('app', 'Created by'),
+            'updated_by' => Yii::t('app', 'Updated by'),
+            'created_at' => Yii::t('app', 'Created at'),
+            'updated_at' => Yii::t('app', 'Updated at'),
+        ];
+    }
+
+    /**
+     * Gets query for [[CreatedBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCreatedBy()
+    {
+        return $this->hasOne(ForumModule::getInstance()->userClass, ['id' => 'created_by']);
+    }
+
+    /**
+     * Gets query for [[UpdatedBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUpdatedBy()
+    {
+        return $this->hasOne(ForumModule::getInstance()->userClass, ['id' => 'updated_by']);
+    }
+
+    /**
+     * Gets query for [[Posts]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPosts()
+    {
+        return $this->hasMany(Post::class, ['created_by' => 'id']);
+    }
+}
