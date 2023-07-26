0ee16875c2370a195dff515d64a32e8694e311d3
Tondina <antoninkazda@seznam.cz>
2023-07-26 20:14:45 +0200

    Add Threads default sort by pinned

--- src/controllers/ForumController.php
+++ src/controllers/ForumController.php
@@ -49,7 +49,8 @@ class ForumController extends Controller
             ],
         ]);
 
-        $query = $forum->getThreads()->andWhere(['status' => [Thread::STATUS_ACTIVE_LOCKED, Thread::STATUS_ACTIVE_UNLOCKED]])->orderBy($sort->orders);
+        $query = $forum->getThreads()->andWhere(['status' => [Thread::STATUS_ACTIVE_LOCKED, Thread::STATUS_ACTIVE_UNLOCKED]])
+            ->orderBy(empty($sort->orders) ? 'pinned DESC' : $sort->orders);
         $countQuery = clone $query;
         $pages = new Pagination(['totalCount' => $countQuery->count(), 'pageSize' => $this->page_items]);
         $models = $query->offset($pages->offset)
