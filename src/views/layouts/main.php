cfecf58a3d902ccf235bee57699ba70cd8bc6c55
Tondina <antoninkazda@seznam.cz>
2023-07-30 16:03:15 +0200

    Fix absolute path in main layout

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -49,7 +49,7 @@ $this->title = $this->title ?? Yii::t('app', 'Forum');
         $navItems = [];
 
         if (Yii::$app->user->can('forum-admin') || Yii::$app->user->can('forum-moderator')) {
-            $navItems[] = ['label' => Yii::t('app', 'Admin'), 'url' => ['/forum/admin/']];
+            $navItems[] = ['label' => Yii::t('app', 'Admin'), 'url' => ['/' . ForumModule::getInstance()->id . '/admin/']];
         }
 
         $navItems[] =
