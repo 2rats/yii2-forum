f07a42cfbff47414c9b429982e2c2633fb8516df
Tondina <antoninkazda@seznam.cz>
2023-07-30 19:58:54 +0200

    Remove svg carrets for sorting

--- src/views/forum/view.php
+++ src/views/forum/view.php
@@ -21,14 +21,7 @@ function getSortLink($sort, $label, $name)
     $class = 'link-secondary link-underline-opacity-0 link-underline-opacity-100-hover ';
     $sort_attr = str_replace('-', '', Yii::$app->request->get('sort', ''));
     if ($sort_attr == $name) {
-        $carret_up = '<svg class="ms-1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-up-fill" viewBox="0 0 16 16"> <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/> </svg>';
-        $carret_down = '<svg class="ms-1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16"> <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/> </svg>';
         $class .= 'fw-medium';
-        if (mb_substr(Yii::$app->request->get('sort', ''), 0, 1) == '-') {
-            $label .= $carret_down;
-        } else {
-            $label .= $carret_up;
-        }
     }
     return $sort->link($name, [
         'class' => $class,
