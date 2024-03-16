f81c4006d47374a69d230d133ecdcc258298d733
Antonín Kazda <antoninkazda@seznam.cz>
2024-03-16 18:03:29 +0100

    fix: search models condition

--- src/models/search/ForumSearch.php
+++ src/models/search/ForumSearch.php
@@ -50,12 +50,6 @@ class ForumSearch extends Forum
 
         $this->load($params);
 
-        if (!$this->validate()) {
-            // uncomment the following line if you do not want to return any records when validation fails
-            // $query->where('0=1');
-            return $dataProvider;
-        }
-
         // grid filtering conditions
         $query->andFilterWhere([
             'id' => $this->id,
