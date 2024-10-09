1d6c709d366b1d510ce77e10391c133c789eaf51
Antonín Kazda <antoninkazda@seznam.cz>
2024-10-09 16:53:56 +0200

    feat: add last_post index

---
+++ src/migrations/m241009_161808_add_last_post_index_to_thread_table.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @author kazda01, mifka01
+ */
+
+use yii\db\Migration;
+
+class m241009_161808_add_last_post_index_to_thread_table extends Migration
+{
+    private $table_name = 'forum_thread';
+
+    public function safeUp()
+    {
+        $this->createIndex(
+            "{{%idx-{$this->table_name}-fk_last_post}}",
+            "{{%{$this->table_name}}}",
+            'fk_last_post'
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->dropIndex(
+            "{{%idx-{$this->table_name}-fk_last_post}}",
+            "{{%{$this->table_name}}}"
+        );
+    }
+}
